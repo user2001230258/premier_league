aebf7879a888f05591a526dd8e80c63480a8d83f
Hoang Van Hoan <2001230258@hufi.edu.vn>
2025-02-14 22:41:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Cấu hình API key và các thông số khác
-FOOTBALL_DATA_API_KEY = 'feef8e39504f4056bcf0dde534a99fb4'  # Thay thế bằng API key của bạn
-PREMIER_LEAGUE_ID = 2021  # ID của Premier League trong football-data.org
+FOOTBALL_DATA_API_KEY = 'feef8e39504f4056bcf0dde534a99fb4'  # API key của bạn
+PREMIER_LEAGUE_ID = 'PL'  # ID mới của Premier League trong football-data.org v4
 UPDATE_INTERVAL = 60  # Thời gian cập nhật dữ liệu (giây)
