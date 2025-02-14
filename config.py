d06b6f867a53aa872e04293f22119e2a5cec5a58
Hoang Van Hoan <2001230258@hufi.edu.vn>
2025-02-14 23:34:10 +0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-FOOTBALL_DATA_API_KEY = os.getenv('FOOTBALL_DATA_API_KEY')
-PREMIER_LEAGUE_ID = 'PL'
-UPDATE_INTERVAL = 60  # Cập nhật mỗi 60 giây
